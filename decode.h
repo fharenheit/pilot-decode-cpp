67f608d2d07c1f5d6877465bdb3643fef33f0732
Edward KIM <fharenheit@gmail.com>
2022-06-23 08:03:08 +0900

    Create decode.h

---
+++ decode.h
@@ -0,0 +1,12 @@
+//
+// Created by 김병곤 on 2022/06/23.
+//
+
+#ifndef TRACE_DECODE_DECODE_H
+#define TRACE_DECODE_DECODE_H
+
+#include <string>
+
+std::string decode(std::string const &encoded, std::string const &write_path);
+
+#endif //TRACE_DECODE_DECODE_H
