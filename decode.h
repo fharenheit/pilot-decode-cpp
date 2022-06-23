858e4a3bf46500e1880cf8754552382dc0483286
KIM BYOUNG GON <fharenheit@gmail.com>
2022-06-24 00:03:41 +0900

    Snappy Success

--- decode.h
+++ decode.h
@@ -13,8 +13,16 @@
 
 using namespace std;
 
+/// Base64 Encoding + Snappy Compression된 문자열을 디코딩한다.
+/// \param encoded
+/// \param write_path
+/// \return
 string decode(string const &encoded, string const &write_path);
 
+/// 지정한 delimiter로 CSV 구조의 토큰 배열로 구성한다.
+/// \param data
+/// \param delimiter
+/// \return
 vector <string> split_by_delimiter(string data, char delimiter);
 
 #endif //TRACE_DECODE_DECODE_H
