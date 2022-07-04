9198cf03f206bf29806bf3e4030d9bba1fe807b0
KIM BYOUNG GON <fharenheit@gmail.com>
2022-07-04 21:50:06 +0900

    Change Make

--- decode.cpp
+++ decode.cpp
@@ -5,8 +5,18 @@
 #include "base64.h"
 #include "snappy.h"
 #include <fstream>
+#include "java.h"
 
 using namespace std;
+
+JNIEXPORT jstring JNICALL Java_com_samsung_ds_tracemon_worker_util_Decoder_decode(JNIEnv *env, jobject obj, jstring string) {
+    std::string encoded = env->GetStringUTFChars(string, NULL);
+    std::string decoded = base64_decode(encoded);
+    std::string result;
+    snappy::Uncompress(decoded.data(), decoded.size(), &result);
+    std::cout << result << std::endl;
+    return env->NewStringUTF(result.data());
+}
 
 string decode(const string &encoded, const string &write_path) {
     ofstream encoded_csv_file(write_path);
@@ -19,10 +29,10 @@ string decode(const string &encoded, const string &write_path) {
     return result;
 }
 
-vector <string> split_by_delimiter(string data, char delimiter) {
+vector<string> split_by_delimiter(string data, char delimiter) {
     istringstream iss(data);
     string buffer;
-    vector <string> result;
+    vector<string> result;
     while (getline(iss, buffer, delimiter)) {
         result.push_back(buffer);
     }
